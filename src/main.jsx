da698cdbf4721ddb43fbc7c3601af44d5ba11ad7
Romina <ro235.g1@gmail.com>
2025-04-09 22:15:16 -0400

    Configuracion

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store.js'
-const basename = import.meta.env.MODE === 'production' ? '/e-plantShopping' : '/';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-    <BrowserRouter basename={basename}>
-    <App />
-  </BrowserRouter>
+    <HashRouter basename="/e-plantShopping">
+        <App />
+    </HashRouter>
     </Provider>
   </React.StrictMode>,
 )
