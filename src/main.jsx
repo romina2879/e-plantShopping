bce6aab03aafa677bce9146fbd5370ee6faffce8
romina2879 <ro235.g1@gmail.com>
2025-04-09 22:41:56 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store.js'
+const basename = import.meta.env.MODE === 'production' ? '/e-plantShopping' : '/';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
+    <BrowserRouter basename={basename}>
     <App />
+  </BrowserRouter>
     </Provider>
   </React.StrictMode>,
 )
